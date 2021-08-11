bca46d8825226ac4c87f75b2c88e2167a8075e05
zero9230 <mengnianyang9230@163.com>
2021-08-11 11:34:46 +0800

    调试

---
+++ Threekingdom11-starter/src/main/java/nianyang/mny/test/ReadDaoImplTest.java
@@ -0,0 +1,36 @@
+package nianyang.mny.test;
+
+import javax.annotation.Resource;
+
+//import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author sikou
+ * @date 2021/08/09
+ */
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration({"classpath:applicationContext.xml"})
+public class ReadDaoImplTest {
+    //@Resource
+    //ReadDao readDao;
+    //
+    //@Test
+    //public void getListTest(){
+    //    List<Client> clientList = readDao.getList("client.test", null);
+    //
+    //    for(Client c:clientList){
+    //        System.out.println(c.getVersionNum());
+    //    }
+    //}
+    //
+    //public  static void main(String[] args){
+    //    ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+    //
+    //    context.start();
+    //
+    //    ReadDao fqaService = (ReadDao) context.getBean("readDao");
+    //    System.out.println(fqaService);
+    //}
+
+}
+
